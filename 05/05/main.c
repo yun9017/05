daa1d19f67b3e989174cdd08ecf7becc321e0b1f
yun9017 <yun9017ie@naver.com>
2025-10-02 15:34:28 +0900

    4

--- 05/05/main.c
+++ 05/05/main.c
@@ -9,16 +9,16 @@
 
 int main(int argc, char * argv[]) {
     int x;
+    int i;
+    int sum=0;
     
     printf("Enter an interger: ");
     scanf("%d", &x);
     
-    if (x>0){
-        printf("absolute is %i.\n",x);
-    }
-    else if (x<0){
-        printf("absolute is %i.\n",-x);
-    }
+    for(i = 1 ;i <= x; i++)
+        sum = sum + i;
+        
+    printf("sum is %i\n", sum);
     
     return 0;
 }
